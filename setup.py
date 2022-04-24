d40ab8c47d05249743769a5f2e7ca3be6782932c
Bilal Ahmed <64080178+bilalhsp@users.noreply.github.com>
2022-04-24 18:55:33 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VAE",
+    version="0.0.1",
+    author="Bilal Ahmed",
+    author_email="ahmedb@purdue.edu",
+    description="Variational Autocoders in PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bilalhsp/",
+    packages=setuptools.find_packages(),
+    package_data={
+        'VAE': [
+            'conf/params.yaml'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib', 'pandas', 'torchvision', 'transformers'
+    ],
+)
